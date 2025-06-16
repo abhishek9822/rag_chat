1ade7cda3d07412c8823ec0376a16a1245bcb04c
abhishek9822 <sharmaabhishek9822@gmail.com>
2025-06-16 18:37:46 +0530

    constant file

---
+++ constant/constant.py
@@ -0,0 +1,10 @@
+import os 
+
+RAW_DATA = "data/raw/test.txt"
+CONVERSATION_DATA = "data/conversation_history"
+COLLECTION_NAME = "my_docs"
+
+NAMESPACE ="my_docs_namespace"
+
+CHUNK_SIZE: int = 1000
+CHUNK_OVERLAP: int = 200
